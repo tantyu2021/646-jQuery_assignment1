7dafa980504c7254be802013123137dcf9948fda
tantyu2021 <tantyu2021@gmail.com>
2020-03-29 23:33:22 -0700

    style headings - h2 - in external script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 $(function() {
-            window.alert('the page is ready');
+           // window.alert('the page is ready');
+
+            $('h2').css({
+                'border': 'black 1px solid',
+                //'color': '#CC1C0D',
+                'padding': '3px 0 3px 20px', 
+                //'border-radius': '0 5px 0 5px',
+                'background-color': '#DFE3E6'
+
+            });
+ //or
+        //parent-child selector
+        $('section > h2, aside > h2').css('color','green');
+ //or           
+        //:header   -$(":header") -select all h1 -h4,etc
+        $(':header').css('border-radius','0 5px 0 5px');
+
+
+
         });
+
+       
+
+   
